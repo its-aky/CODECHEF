90a4fc57d7fd28f1922569e9457a86a65504e04f
its-aky <kumaramityadav230@gmail.com>
2023-11-10 20:06:47 +0530

    initial commit

---
+++ CODECHEF_PRACTICE/1379_Make_it_divisible.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define mod 1000000007
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+        ll n;
+        cin>>n;
+
+        string s;
+        int cnt=0;
+        for (int i = 0; i < n-1; i++)
+        {
+            s.push_back('1');
+            cnt++;
+        }
+        int temp[]={1,3,5,7,9};
+        for (int i = 0; i < 5; i++)
+        {
+            if((temp[i]+cnt)%3==0 && (temp[i]+cnt)%9!=0 ){
+                s.push_back((char)temp[i]+48);
+                break;
+            }
+        }
+
+        for (int i = 0; i < s.size(); i++)
+        {
+            cout<<s[i];
+        }
+        cout<<endl;
+    }
+}
